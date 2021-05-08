47ef27e0e9f017e7d2d1cd9a4a2780be666f6d13
RYOGA010203 <53912804+RYOGA010203@users.noreply.github.com>
2021-05-08 18:06:16 +0900

    Add files via upload

---
+++ kanto_haru20_movie/src/haru20_b_joy_main.cpp
@@ -0,0 +1,643 @@
+/*
+ * kari.cpp
+ *
+ *  Created on: Aug 19, 2019
+ *      Author: shun
+ */
+
+#include <ros/ros.h>
+#include <std_msgs/UInt8.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Path.h>
+#include <sensor_msgs/Joy.h>
+#include <vector>
+#include <string>
+
+#define pi 3.1416
+
+enum class CarrierStatus
+    : uint16_t
+    {
+        shutdown = 0x0000,
+    reset = 0x0001,
+
+/*
+ operational			= 0x0002,
+
+ chuck0_chucked		= 0x0010,
+ chuck1_chucked		= 0x0020,
+ chuck2_chucked		= 0x0040,
+ chuck3_chucked		= 0x0080,
+ */
+};
+
+enum class CarrierCommands
+    : uint16_t
+    {
+        shutdown_cmd = 0x0000,
+    reset_cmd = 0x0001,
+    /*
+     operational			= 0x0002,
+     */
+
+    chuck_cmd = 0x0100,
+    unchuck_cmd = 0x0200,
+
+    chuck0 = 0x0010,
+    chuck1 = 0x0020,
+    chuck2 = 0x0040,
+    chuck3 = 0x0080,
+};
+
+class CrMain
+{
+public:
+    CrMain(void);
+
+private:
+    void shutdownInputCallback(const std_msgs::Empty::ConstPtr& msg);
+    void startInputCallback(const std_msgs::Empty::ConstPtr& msg);
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+	void PosCallback(const std_msgs::Float32::ConstPtr& msg);
+
+    ros::NodeHandle nh_;
+
+    //int linear_, angular_;
+    ros::Subscriber joy_sub;
+    ros::Subscriber shutdown_input_sub;
+    ros::Subscriber start_input_sub;
+
+	/***********************/
+	ros::Subscriber throw_Pos_sub;
+	/***********************/
+
+    ros::Publisher pick_position_pub;
+    ros::Publisher pick_enable_pub;
+
+    ros::Publisher throw_position_pub;
+    ros::Publisher throw_enable_pub;
+
+	/***********************/
+	ros::Publisher solenoid_position_pub;
+    ros::Publisher solenoid_enable_pub;
+	/***********************/
+
+    ros::Publisher act_enable_pub0;
+    ros::Publisher act_enable_pub1;
+    ros::Publisher act_enable_pub2;
+	/**********************/
+	ros::Publisher act_enable_pub3;
+	/**********************/
+
+    std_msgs::Float32 pick_position_msg;
+    std_msgs::Float32 throw_position_msg;
+	/**********************/
+	std_msgs::Float32 throw_Pos_msg;
+	/**********************/
+
+	/**********************/
+	std_msgs::UInt8 solenoid_position_msg;
+	/**********************/
+    std_msgs::UInt8 act_enable_msg;
+
+	/**********************/
+	std_msgs::UInt8 shirasu_cmd_msg;
+	/**********************/
+
+    double steps_per_mm = 1;
+
+    std::vector<double> pick_position = { 0, pi/2, 2*pi/3, };
+    int pick_position_index = 0;
+
+    std::vector<double> throw_position = { 0, 2*pi, -2*pi };
+    int throw_position_index = 0;
+
+	double throw_position_real = 0;
+
+	long int throw2_position_real = 0;
+
+	int dr_mode = 0;
+	// 0 : taiki_mode
+	// 1 : genten_awase_mode
+	// 2 : haji_mode
+	// 3 : tohteki_mode
+	// 4 : defence_mode
+
+	/************************/
+	unsigned char solenoid_data = 0;
+	/************************/
+
+    //		{0, -40 * steps_per_mm;
+    //static constexpr int lift_position_first = -40 * steps_per_mm;
+    //static constexpr int lift_position_second = lift_position_first - (248 * steps_per_mm);
+    //static constexpr int lift_position_third = lift_position_second - (248 * steps_per_mm);
+
+    bool _shutdown = true;
+
+    static int Button1;
+    static int Button2;
+    static int Button3;
+    static int Button4;
+    static int Button5;
+    static int Button6;
+    static int Button11;
+    static int Button12;
+	/***************/
+	static int Button9;
+    static int Button10;
+	/***************/
+    static int Button7;
+    static int Button8;
+
+    static int AxisDPadX;
+    static int AxisDPadY;
+};
+
+int CrMain::Button1 = 0;
+int CrMain::Button2 = 1;
+int CrMain::Button3 = 2;
+int CrMain::Button4 = 3;
+int CrMain::Button5 = 4;
+int CrMain::Button6 = 5;
+int CrMain::Button7 = 6;
+int CrMain::Button8 = 7;
+/******************/
+int CrMain::Button9 = 8;
+int CrMain::Button10 = 9;
+/******************/
+int CrMain::Button11 = 10;
+int CrMain::Button12 = 11;
+
+int CrMain::AxisDPadX = 4;
+int CrMain::AxisDPadY = 5;
+
+CrMain::CrMain(void)
+{
+    joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &CrMain::joyCallback, this);
+    shutdown_input_sub = nh_.subscribe<std_msgs::Empty>("shutdown_input", 10, &CrMain::shutdownInputCallback, this);
+    start_input_sub = nh_.subscribe<std_msgs::Empty>("start_input", 10, &CrMain::startInputCallback, this);
+
+	/***************************************/
+	throw_Pos_sub = nh_.subscribe<std_msgs::Float32>("throw/motorth_Pos", 10, &CrMain::PosCallback, this);
+	/***************************************/
+
+    this->throw_position_pub = nh_.advertise<std_msgs::Float32>("throw/motorth_cmd_pos", 1);
+    this->throw_enable_pub = nh_.advertise<std_msgs::UInt8>("throw/motorth_cmd", 1);
+
+    this->pick_position_pub = nh_.advertise<std_msgs::Float32>("pick/motorpc_cmd_pos", 1);
+    this->pick_enable_pub = nh_.advertise<std_msgs::UInt8>("pick/motorpc_cmd", 1);
+
+	/**************************************************/
+	this->solenoid_position_pub = nh_.advertise<std_msgs::UInt8>("solenoid/motorsl_cmd_pos", 1);
+    this->solenoid_enable_pub = nh_.advertise<std_msgs::UInt8>("solenoid/motorsl_cmd", 1);
+	/**************************************************/
+
+    this->act_enable_pub0 = nh_.advertise<std_msgs::UInt8>("base/motor0_cmd", 1);
+    this->act_enable_pub1 = nh_.advertise<std_msgs::UInt8>("base/motor1_cmd", 1);
+    this->act_enable_pub2 = nh_.advertise<std_msgs::UInt8>("base/motor2_cmd", 1);
+	/**************************************************/
+	this->act_enable_pub3 = nh_.advertise<std_msgs::UInt8>("base/motor3_cmd", 1);
+	/**************************************************/
+    //this->hand_unchuck_thres_pub = nh_.advertise<std_msgs::UInt16>("hand/unchuck_thres", 1);
+
+    auto nh_priv = ros::NodeHandle("~");
+
+    /*nh_priv.getParam("lift_step_per_mm", this->steps_per_mm);
+
+    std::vector<double> tmp;
+    nh_priv.getParam("pick_position", tmp);
+    if (tmp.size() == 5)
+    {
+        this->pick_position = tmp;
+    }
+
+    for (double& pos : this->pick_position)
+    {
+        pos *= (-steps_per_mm);
+    }
+
+    ROS_INFO("pick_pos: %f, %f, %f, %f, %f", this->pick_position[0], this->pick_position[1], this->pick_position[2],
+            this->pick_position[3], this->pick_position[4]);*/
+
+	/************************************************************
+	std::vector<int> tmp2;
+    nh_priv.getParam("solenoid_position", tmp2);
+    if (tmp2.size() == 5)
+    {
+        this->solenoid_position = tmp2;
+    }
+
+    for (int& pos : this->solenoid_position)
+    {
+        pos *= (-steps_per_mm);
+    }
+
+    ROS_INFO("solenoid_pos: %d, %d, %d, %d, %d", this->solenoid_position[0], this->solenoid_position[1], this->solenoid_position[2],
+            this->solenoid_position[3], this->solenoid_position[4]);
+	************************************************************/
+
+    nh_.getParam("Button1", Button1);
+    nh_.getParam("Button2", Button2);
+    nh_.getParam("Button3", Button3);
+    nh_.getParam("Button4", Button4);
+    nh_.getParam("Button5", Button5);
+    nh_.getParam("Button6", Button6);
+    nh_.getParam("Button11", Button11);
+    nh_.getParam("Button12", Button12);
+	/***************************/
+	nh_.getParam("Button9", Button9);
+    nh_.getParam("Button10", Button10);
+	/***************************/
+
+    nh_.getParam("Button7", Button7);
+    nh_.getParam("Button8", Button8);
+
+    nh_.getParam("AxisDPadX", AxisDPadX);
+    nh_.getParam("AxisDPadY", AxisDPadY);
+
+}
+
+void CrMain::shutdownInputCallback(const std_msgs::Empty::ConstPtr& msg)
+{
+    if (!this->_shutdown)
+    {
+//        this->_shutdown = 1;
+
+//        ROS_INFO("aborting.");
+    }
+
+    // reset this:
+    // this->CurrentCommandIndex = -1;
+    pick_position_index = 0;
+}
+
+void CrMain::startInputCallback(const std_msgs::Empty::ConstPtr& msg)
+{
+    // bring the robot back operational
+
+    ROS_INFO("starting.");
+
+    act_enable_msg.data = 1;
+    act_enable_pub0.publish(act_enable_msg);
+    act_enable_pub1.publish(act_enable_msg);
+    act_enable_pub2.publish(act_enable_msg);
+	act_enable_pub3.publish(act_enable_msg);
+    pick_enable_pub.publish(act_enable_msg);
+    throw_enable_pub.publish(act_enable_msg);
+	/******************************/
+	solenoid_enable_pub.publish(act_enable_msg);
+	/******************************/
+    this->_shutdown = 0;
+}
+/**************************************************************************************/
+void CrMain::PosCallback(const std_msgs::Float32::ConstPtr& msg)
+{
+	throw_position_real = msg->data;
+}
+/**************************************************************************************/
+void CrMain::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+    static bool last_1 = false;
+    static bool last_2 = false;
+    static bool last_3 = false;
+    static bool last_4 = false;
+    static bool last_5 = false;
+    static bool last_6 = false;
+    static bool last_11 = false;
+    static bool last_12 = false;
+    static bool last_7 = false;
+    static bool last_8 = false;
+	/****************/
+	static bool last_9 = false;
+    static bool last_10 = false;
+	/****************/
+
+	static char last_13 = false;
+	static char last_14 = false;
+
+
+    //static int last_dpadXCmd = 0;
+
+    bool _1 = joy->buttons[Button1];
+    bool _2 = joy->buttons[Button2];
+    bool _3 = joy->buttons[Button3];
+    bool _4 = joy->buttons[Button4];
+    bool _5 = joy->buttons[Button5];
+    bool _6 = joy->buttons[Button6];
+    bool _11 = joy->buttons[Button11];
+    bool _12 = joy->buttons[Button12];
+	/****************/
+	bool _9 = joy->buttons[Button9];
+    bool _10 = joy->buttons[Button10];
+	/****************/
+    bool _7 = joy->buttons[Button7];
+    bool _8 = joy->buttons[Button8];
+
+	char _14 = joy->axes[AxisDPadX];
+    char _13 = joy->axes[AxisDPadY];
+
+    if (_12 && !last_12)
+    {
+        ROS_INFO("all enable          (button12:AxisRightTrigger)");
+
+        act_enable_msg.data = 1;
+        act_enable_pub0.publish(act_enable_msg);
+        act_enable_pub1.publish(act_enable_msg);
+        act_enable_pub2.publish(act_enable_msg);
+		act_enable_pub3.publish(act_enable_msg);
+        pick_enable_pub.publish(act_enable_msg);
+        throw_enable_pub.publish(act_enable_msg);
+		/****************************/
+		solenoid_enable_pub.publish(act_enable_msg);
+		/****************************/
+
+        this->_shutdown = 0;  
+    }
+
+
+    if (_11 && !last_11)
+    {
+        if (!this->_shutdown)
+        {
+            this->_shutdown = 1;
+
+            ROS_INFO("all disable & stop  (button11:AxisLeftTrigger)");
+        }
+        
+        act_enable_msg.data = 0;
+        act_enable_pub0.publish(act_enable_msg);
+        act_enable_pub1.publish(act_enable_msg);
+        act_enable_pub2.publish(act_enable_msg);
+		act_enable_pub3.publish(act_enable_msg);
+        pick_enable_pub.publish(act_enable_msg);
+        throw_enable_pub.publish(act_enable_msg);
+		/************************/
+		solenoid_enable_pub.publish(act_enable_msg);
+		/************************/
+    }
+
+    if (!this->_shutdown)
+    {
+        if (_7 && !last_7)
+        {
+            /*// receive  the throw
+            throw_position_index = 1;
+            throw_position_msg.data = throw_position[throw_position_index];
+            throw_position_pub.publish(throw_position_msg);*/
+
+			ROS_INFO("rotation Left       (button7:ButtonLeftThumb)");
+        }
+        else if (_8 && !last_8)
+        {
+            /*//  the throw
+            throw_position_index = 2;
+            throw_position_msg.data = throw_position[throw_position_index];
+            throw_position_pub.publish(throw_position_msg);*/
+
+			ROS_INFO("rotation Right      (button8:ButtonRightThumb)");
+        }
+       else if (_4 && !last_4)
+        {
+        	if ( (solenoid_data & 0b00000001) == 0b00000000 )
+        	{
+            	// solenoid ch0 on
+            	solenoid_data = (solenoid_data | 0b00000001);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch0 on     (button4:ButtonY)");
+            }
+            
+            else
+            {
+            	// solenoid ch0 off
+            	solenoid_data = (solenoid_data & 0b11111110);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch0 off    (button4:ButtonY)");
+            }
+
+        }
+        else if (_3 && !last_3)
+        {
+            if ( (solenoid_data & 0b00000010) == 0b00000000 )
+        	{
+            	// solenoid ch1 on
+            	solenoid_data = (solenoid_data | 0b00000010);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch1 on     (button3:ButtonB)");
+            }
+            
+            else
+            {
+            	// solenoid ch1 off
+            	solenoid_data = (solenoid_data & 0b11111101);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch1 off    (button3:ButtonB)");
+            	}
+        }
+        else if (_2 && !last_2)
+        {
+            if ( (solenoid_data & 0b00000100) == 0b00000000 )
+        	{
+            	// solenoid ch2 on
+            	solenoid_data = (solenoid_data | 0b00000100);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch2 on     (button2:ButtonA)");
+            }
+            
+            else
+            {
+            	// solenoid ch2 off
+            	solenoid_data = (solenoid_data & 0b11111011);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch2 off    (button2:ButtonA)");
+            }
+        }
+        else if (_1 && !last_1)
+        {
+            if ( (solenoid_data & 0b00001000) == 0b00000000 )
+        	{
+            	// solenoid ch3 on
+            	solenoid_data = (solenoid_data | 0b00001000);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch3 on     (button1:ButtonX)");
+            }
+            
+            else
+            {
+            	// solenoid ch3 off
+            	solenoid_data = (solenoid_data & 0b11110111);
+            	solenoid_position_msg.data = solenoid_data;
+            	solenoid_position_pub.publish(solenoid_position_msg);
+
+            	ROS_INFO("solenoid ch3 off    (button1:ButtonX)");
+            }
+        }
+		/******************************************/
+		else if (_5 && !last_5)
+        {
+			/*****/
+			throw_position_index = 0;
+            throw_position_msg.data = throw_position[throw_position_index];
+            throw_position_pub.publish(throw_position_msg);
+			/*****/
+			shirasu_cmd_msg.data = 1;
+			throw_enable_pub.publish(shirasu_cmd_msg);
+
+			ROS_INFO("shirasu vel         (button5:ButtonLB)");
+        }
+        else if (_6 && !last_6)
+        {
+			/*****/
+			throw_position_index = 0;
+            throw_position_msg.data = throw_position[throw_position_index];
+            throw_position_pub.publish(throw_position_msg);
+			/*****/
+            shirasu_cmd_msg.data = 6;
+			throw_enable_pub.publish(shirasu_cmd_msg);
+
+			ROS_INFO("shirasu pos         (button6:ButtonRB)");
+        }
+		/*******************************************/
+        else if (_13 && !last_13)
+        {
+            if(_13 == 1)
+			{
+				if(shirasu_cmd_msg.data == 1)
+					{
+					// set the pick
+            		pick_position_index = 0;
+            		pick_position_msg.data = pick_position[pick_position_index];
+            		throw_position_pub.publish(pick_position_msg);
+
+            		ROS_INFO("vel Index 0         (button13+:AxisDPadX)");
+					}
+			}
+			else if(_13 == -1)
+			{
+				/*
+				if(shirasu_cmd_msg.data == 1)
+				{
+					// set the pick
+            		pick_position_index = 2;
+            		pick_position_msg.data = pick_position[pick_position_index];
+            		throw_position_pub.publish(pick_position_msg);
+
+            		ROS_INFO("vel Index 2         (button13-:AxisDPadX)");
+				}
+				*/
+				if(shirasu_cmd_msg.data == 6)
+					{
+						// warizan
+						throw2_position_real = (long int)( throw_position_real * 1000 );
+						throw_position[0] = ( ( (double)throw2_position_real ) ) / 1000;
+						throw_position[1] = ( ( (double)throw2_position_real ) + 500*pi )/ 1000;
+						throw_position[2] = ( ( (double)throw2_position_real ) + 1500*pi )/ 1000;
+						ROS_INFO("vector update       (button14-:AxisDPadX)");
+					}
+			}
+        }
+        else if (_14 && !last_14)
+        {
+            if(_14 == 1)
+			{
+				if(shirasu_cmd_msg.data == 1)
+				{
+					// serve the pick
+            		pick_position_index = 2;
+            		pick_position_msg.data = pick_position[pick_position_index];
+            		throw_position_pub.publish(pick_position_msg);
+
+            		ROS_INFO("vel Index 2         (button14+:AxisDPadY)");
+				}
+			}
+			else if(_14 == -1)
+			{
+				if(shirasu_cmd_msg.data == 1)
+				{
+					// serve the pick
+            		pick_position_index = 1;
+            		pick_position_msg.data = pick_position[pick_position_index];
+            		throw_position_pub.publish(pick_position_msg);
+
+            		ROS_INFO("vel Index 1         (button14-:AxisDPadY)");
+				}
+			}
+        }
+		/*********************************************/
+
+		else if (_9 && !last_9)
+        {
+			if(shirasu_cmd_msg.data == 6)
+			{
+				throw_position_index = 1;
+            	throw_position_msg.data = throw_position[throw_position_index];
+            	throw_position_pub.publish(throw_position_msg);
+
+            	ROS_INFO("pos Index 1         (button9:ButtonBack)");
+			}
+        }
+		else if (_10 && !last_10)
+        {
+			if(shirasu_cmd_msg.data == 6)
+			{
+				throw_position_index = 2;
+            	throw_position_msg.data = throw_position[throw_position_index];
+            	throw_position_pub.publish(throw_position_msg);
+
+				ROS_INFO("pos Index 2         (button10:ButtonStart)");
+			}
+        }
+		/********************************************/
+    }
+
+
+    last_1 = _1;
+    last_2 = _2;
+    last_3 = _3;
+    last_4 = _4;
+    last_5 = _5;
+    last_6 = _6;
+    last_7 = _7;
+    last_8 = _8;
+	/******/
+	last_9 = _9;
+    last_10 = _10;
+	/******/
+    last_11 = _11;
+    last_12 = _12;
+
+	last_13 = _13;
+	last_14 = _14;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "natu19_b_joy_main");
+
+    CrMain *crMain = new CrMain();
+    ROS_INFO("natu19_b_joy_main node has started.");
+
+    ros::spin();
+    ROS_INFO("natu19_b_joy_main node has been terminated.");
+}
+
